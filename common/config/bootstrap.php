23ba0c1959bd3298582d1ce8b667348998e068d1
digitalbrainleb-cloud <digitalbrain.leb@gmail.com>
2025-12-28 22:04:33 +0200

    original

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
-// Detect environment
-if (getenv('YII_ENV') === 'prod') {
-    defined('YII_DEBUG') or define('YII_DEBUG', false);
-    defined('YII_ENV') or define('YII_ENV', 'prod');
-} else {
-    defined('YII_DEBUG') or define('YII_DEBUG', true);
-    defined('YII_ENV') or define('YII_ENV', 'dev');
-}
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
